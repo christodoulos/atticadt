d5048ac24e8804191c42c486ca25c15612921d63
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-01-04 09:33:23 +0200

    cleanup

--- libs/mapbox/src/lib/map/map.component.ts
+++ libs/mapbox/src/lib/map/map.component.ts
@@ -6,7 +6,6 @@ import {
   ViewChild,
 } from '@angular/core';
 import { MapService } from '../map.service';
-import { MapState } from '../state';
 
 @Component({
   selector: 'uwmh-map',
@@ -16,7 +15,7 @@ import { MapState } from '../state';
 export class MapComponent implements AfterViewInit {
   @ViewChild('map') mapDiv!: ElementRef;
 
-  constructor(private service: MapService, private state: MapState) {}
+  constructor(private service: MapService) {}
 
   ngAfterViewInit(): void {
     const { map, tb } = this.service.newMap(this.mapDiv);
